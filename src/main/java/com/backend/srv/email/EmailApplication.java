c97513862f4ed559dfb8437f6ff985ceb652172e
Daniel Pereira do Nascimento <daniel.nascimento@etipi.pi.gov.br>
2024-09-21 22:37:12 -0300

    fix: env variables

--- src/main/java/com/backend/srv/email/EmailApplication.java
+++ src/main/java/com/backend/srv/email/EmailApplication.java
@@ -2,6 +2,7 @@ package com.backend.srv.email;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailApplication {
 
 	private final Logger logger = LogManager.getLogger(this.getClass());
+
+	@Value("${logstash.host}")
+    private String logstashHost;
+
+    @Value("${logstash.port}")
+    private String logstashPort;
 
 	public static void main(String[] args) {
 		EmailApplication app = new EmailApplication();
@@ -20,6 +27,8 @@ public class EmailApplication {
 		logger.info("Iniciando a aplicação de e-mail");
 		SpringApplication.run(EmailApplication.class, args);
 		logger.info("Aplicação de e-mail iniciada com sucesso");
+		logger.info("Logstash host: {}", logstashHost);
+		logger.info("Logstash port: {}", logstashPort);
 	}
 
 }
